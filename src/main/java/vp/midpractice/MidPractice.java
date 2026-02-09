3f511ae725fa881ce93c3070c2ddfed3878e90f2
Emir <emirm64@gmail.com>
2026-02-09 12:44:08 -0500

    practice 1

---
+++ src/main/java/vp/midpractice/MidPractice.java
@@ -0,0 +1,16 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vp.midpractice;
+
+/**
+ *
+ * @author cod-4
+ */
+public class MidPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
